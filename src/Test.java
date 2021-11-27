cfe91cfd22a32f2f2ef652a5dda671664ab06f6c
Ali <ali-alsadah1941@hotmail.com>
2021-11-27 22:35:13 +0300

    write initial insert method, and insert the permutations in the trie

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Test {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("dictionary.txt"));
+        Scanner input = new Scanner(System.in);
+
+        //promote the user to enter the list of letters or a word
+        System.out.print("Enter your list of letters> ");
+        String[] letters = input.nextLine().split("");
+        Arrays.sort(letters);
+
+        String str = String.join("", letters).toLowerCase();
+
+        Trie trie = new Trie();
+        trie.initialInsert(str);
+
+        while(scanner.hasNext()){
+            String s1 = scanner.nextLine().toLowerCase();
+
+            String[] s = s1.split("");
+            Arrays.sort(s);
+            String word = String.join("", s).toLowerCase();
+
+            if(str.equalsIgnoreCase(word))
+                trie.insert(s1);
+        }
+
+
     }
+    public static List<String> permutations(String s) {
+
+        List<String>combinations=new ArrayList<String>();
+        if(s.length()==1){
+            combinations.add(s);
+        }
+        else{
+            for(int i=0;i<s.length();i++){
+                List<String>temp=permutations(s.substring(0, i)+s.substring(i+1));
+                for (String string : temp) {
+                    combinations.add(s.charAt(i)+string);
+                }
+            }
+        }
+        return combinations;
+    }
+
 }
--- src/Trie.java
+++ src/Trie.java
@@ -13,6 +13,17 @@ public class Trie {
                 p.children[c - 'a'] = new TrieNode(c);
             p = p.children[c - 'a'];
         } p.isWord = true;
-
     }
+
+    public void initialInsert(String s){
+        TrieNode p = root;
+        for (int i = 0; i < s.length(); i++){
+            char c  = s.charAt(i);
+            if (p.children[c - 'a'] == null)
+                p.children[c - 'a'] = new TrieNode(c);
+        }
+    }
+
+
+
 }
