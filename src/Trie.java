2b9a1eadde47dc058ac2ed72472720edd5a3b7fb
Ali <ali-alsadah1941@hotmail.com>
2021-11-27 17:01:09 +0300

    first commit

--- src/Trie.java
+++ src/Trie.java
@@ -1,2 +1,18 @@
 public class Trie {
+    private TrieNode root;
+
+    public Trie(){
+        root = new TrieNode('\0');
+    }
+
+    public void insert(String s){
+        TrieNode p = root;
+        for (int i = 0; i < s.length(); i++){
+            char c  = s.charAt(i);
+            if (p.children[c - 'a'] == null)
+                p.children[c - 'a'] = new TrieNode(c);
+            p = p.children[c - 'a'];
+        } p.isWord = true;
+
+    }
 }
--- src/TrieNode.java
+++ src/TrieNode.java
@@ -1,10 +1,12 @@
 public class TrieNode {
 
     protected char el;
+    protected boolean isWord;
     protected TrieNode[] children;
 
     public TrieNode(char el){
         this.el = el;
+        isWord = false;
         children = new TrieNode[26];
     }
 }
